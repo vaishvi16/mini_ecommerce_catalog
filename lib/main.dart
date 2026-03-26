cfa60b6a1b63ceb7c106da521e196a4869ba7ec2
Vaishvi07 <91317372+Vaishvi07@users.noreply.github.com>
2026-03-26 13:11:39 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:mini_ecommerce_catalog/providers/auth_provider.dart';
 import 'package:mini_ecommerce_catalog/providers/cart_provider.dart';
 import 'package:mini_ecommerce_catalog/screens/auth_screens/login_screen.dart';
